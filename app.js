c42a06d9192c21359529d05c182c0615589d8a43
Ricardo Melo Joia <r.joia@audatex.pt>
2016-01-13 13:25:17 +0000

    repo init

---
+++ app.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    var settings = require('./src/config/settings')
+
+    console.info('Start here')
+    console.info('Settings file loaded: ' + settings.config.key);
+
+})();
---
+++ src/config/settings.js
@@ -0,0 +1,14 @@
+var settings = function(){
+    'use strict';
+    
+    var config = {
+        key : 'config Value'
+    }
+    
+    return {
+        config: config
+    }
+    
+}();
+
+module.exports = settings; 
